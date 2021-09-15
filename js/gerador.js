93bcbea1e119455a58d6146c5d11d3733c16c283
Rooney <roneysantos7711@gmail.com>
2021-09-14 23:13:20 -0300

    Add descrição das ideias, Fix padding em dispositivos mobile e novas ideias

--- js/gerador.js
+++ js/gerador.js
@@ -34,40 +34,95 @@ Vue.component('rodape-bottom', {
     </footer>`,
 })
 
-//To do -  Add mais projeto e uma descrição 
 var app = new Vue({
     el: '#app',
     data: {
         projetoGerado: "",
+        projetoDescricao: "",
         checkedNivel: "",
         checkedCategoria: "",
         simples: {
-            jogos: ["Crie um jogo estilo cobrinha", "Crie um jogo com uso do Lançamento Oblíquo", "Crie um jogo de plataforma"],
-            sites: ["Crie um formulário de cadastro que guarda os dados em variáveis e imprime no console","Desenvolva uma página de noticias", "Desenvolva um site que mostre um vídeo do youtube"],
-            softwares: ["Desenvolva uma calculadora sem interface"],
-            aplicativos: ["Desenvolva um app que some duas entradas para android"],
+            jogos: {
+                titulo: ["Crie um jogo estilo cobrinha", "Crie um jogo com o uso do Lançamento Oblíquo", "Crie um jogo de plataforma"],
+                descricao: ["No jogo a cobrinha não pode colidir consigo mesma ou nas paredes. Também deve conter os item que a cobrinha ira coletar para crescer.",
+                    "O lançamento oblíquo ocorre quando um objeto inicia seu movimento formando um determinado ângulo com a horizontal. Nesse tipo de lançamento, o objeto executa dois movimentos simultâneos, ao mesmo tempo em que executa um movimento na vertical, subindo e descendo, também se desloca horizontalmente.",
+                    "O personagem deve poder andar para a esquerda, direita, pular e coletar moedas."]
+            },
+            sites: {
+                titulo: ["Crie um formulário de cadastro que guarda os dados em variáveis e imprime no console", "Desenvolva uma página de notícias", "Desenvolva uma página que mostre um vídeo do youtube"],
+                descricao: ["As informações são nome, data de nascimento, sexo, RG, CPF, telefone, rua, número, bairro, cidade, estado, cep e complemento.", "Crie uma página que mostre uma notícia atual com data de publicação, autor e fontes.", "Faça uma descrição do vídeo e incorpore o vídeo do youtube dentro da página."]
+            },
+            softwares: {
+                titulo: ["Desenvolva uma calculadora"],
+                descricao: ["Devera ter as funções de somar, subtrair, multiplicar e dividir."]
+            },
+            aplicativos: {
+                titulo: ["Desenvolva uma calculadora para android"],
+                descricao: ["Devera ter as funções de somar, subtrair, multiplicar e dividir."]
+            },
         }, mediano: {
-            jogos: ["Crie um jogo estilo cobrinha com sons, recorde e contador de mortes", "Crie um jogo de plataforma com sons, contador de moedas e contador de mortes", "Crie um jogo simples estilo Endless runner"],
-            sites: ["Desenvolva site de noticias resposivo", "Desenvolva um site de cadastro de pessoas conectado com um banco de dados e que mostre as pessoas cadastradas"],
-            softwares: ["Desenvolva uma calculadora como uma interface", "Crie um programa de cadastro de pessoas conectado com um banco de dados e que mostre as pessoas cadastradas"],
-            aplicativos: ["Desenvolva uma calculadora para android"]
+            jogos: {
+                titulo: ["Crie um jogo estilo cobrinha", "Crie um jogo de plataforma", "Crie um jogo simples estilo Endless runner"],
+                descricao: ["No jogo a cobrinha não pode colidir consigo mesma ou nas paredes. Também deve conter sons, recorde e contador de mortes e os item que a cobrinha ira coletar para crescer.",
+                    "O personagem deve poder andar para a esquerda, direita, pular e coletar moedas. Também deve conter sons, contador de moedas e contador de mortes.",
+                    "O personagem deve poder andar para a esquerda, direita, pular e coletar moedas. Também deve conter sons, contador de moedas e contador de mortes."]
+            },
+            sites: {
+                titulo: ["Desenvolva uma página de noticias responsivo", "Desenvolva um site de cadastro de pessoas responsivo. Conectado com um banco de dados e que mostre as pessoas cadastradas pesquisando pelo nome ou pelo CPF"],
+                descricao: ["Crie uma página que mostre uma notícia atual com data de publicação, autor e fontes. Também incorpore um vídeo do youtube dentro da página.",
+                    "As informações são nome, data de nascimento, sexo, RG, CPF, telefone, rua, número, bairro, cidade, estado, cep e complemento."]
+            },
+            softwares: {
+                titulo: ["Desenvolva uma calculadora para windows", "Crie um programa de cadastro de pessoas. Conectado com um banco de dados e que mostre as pessoas cadastradas pesquisando pelo nome ou pelo CPF para windows"],
+                descricao: ["Devera ter as funções de somar, subtrair, multiplicar e dividir.",
+                    "As informações são nome, data de nascimento, sexo, RG, CPF, telefone, rua, número, bairro, cidade, estado, cep e complemento."]
+            },
+            aplicativos: {
+                titulo: ["Desenvolva um aplicativo de notas"],
+                descricao: ["Deve permitir o usuário criar notas e salva-las para visualizar posteriormente."]
+            }
         }, avancado: {
-            jogos: ["Crie um jogo 3d estilo Endless runner com menu inicial e de pausa, opções de configurações, sons, animações, contador de morte e recorde máximo atingido", "Crie um jogo em 3d de tiro com menu inicial e de pausa, opções de configurações, sons, animações, contador de morte e recorde máximo atingido"],
-            sites: ["Desenvolva um site que busque o tempo conectado a uma API", "Desenvolva um site que busque o endereço pelo CEP conectado a uma API"],
-            softwares: ["Desenvolva uma calculadora"],
-            aplicativos: ["Desenvolva um agregador de GIFs", "Desenvolva um app para visualizar fotos e vídeos", "Desenvolva um aplicativo de notas"]
+            jogos: {
+                titulo: ["Crie um jogo 3d estilo Endless runner", "Crie um jogo em 3d de tiro"],
+                descricao: ["O personagem deve poder andar para a esquerda, direita, pular e coletar moedas. Também deve conter menu inicial e de pausa, opções de configurações, sons, animações, contador de morte e recorde máximo atingido.",
+                    "Com menu inicial e de pausa, opções de configurações, sons, animações, contador de morte e recorde máximo atingido."]
+            },
+            sites: {
+                titulo: ["Desenvolva um site que busque o tempo conectado a uma API", "Desenvolva um site que busque o endereço pelo CEP conectado a uma API"],
+                descricao: ["Deve mostrar a temperatura atual, mínima, máxima e previsão de chuva", "Deve mostrar a rua, bairro, cidade, estado e cep."]
+            },
+            softwares: {
+                titulo: ["Desenvolva um programa para contar os cliques do mouse em segundo plano"],
+                descricao: ["Deve contar os cliques do botão direito, esquerdo e da rodinha."]
+            },
+            aplicativos: {
+                titulo: ["Desenvolva um agregador de GIFs", "Desenvolva um app para visualizar fotos e vídeos"],
+                descricao: ["Deve permitir pesquisar GIFs, buscar por categorias e baixar."]
+            }
         }, godLevel: {
-            jogos: ["Desenvolva um jogo 3d que contenha jogadores online", "Desenvolva um jogo 3d em mundo aberto online com pessoas, veículos e missões"],
-            sites: ["Desenvolva um site de e-commerce com marketplace", "Desenvolva um site de cursos online"],
-            softwares: ["Crie um programa para usar a câmera do smartphone pelo usb e wifi"],
-            aplicativos: ["Desenvolva uma rede social", "Desenvolva um app de mensagens"]
+            jogos: {
+                titulo: ["Desenvolva um jogo 3d que contenha jogadores online", "Desenvolva um jogo 3d em mundo aberto online com pessoas, veículos e missões"],
+                descricao: ["Deve permitir que os jogadores joguem em grupos ou um contra os outros.", "Deve permitir que os jogadores joguem em grupos ou um contra os outros."]
+            },
+            sites: {
+                titulo: ["Desenvolva um site de e-commerce com marketplace", "Desenvolva um site de cursos online"],
+                descricao: ["Deve ter todas as funções de um e-commerce.", "Deve conter atividades interativas."]
+            },
+            softwares: {
+                titulo: ["Crie uma IDE para alguma linguagem de programação"],
+                descricao: ["Deve ter as funções básicas de uma IDE."]
+            },
+            aplicativos: {
+                titulo: ["Desenvolva uma rede social", "Desenvolva um app de mensagens", "Crie um app para usar a câmera do smartphone no computador"],
+                descricao: ["Deve conter todas as funções básicas de uma rede social.", "Deve conter todas as funções básicas de um aplicativo de mensagens instantâneas.", "Deve permitir usar a câmera do smartphone pelo USB no computador ou pela rede local WiFi."]
+            }
         },
     },
     methods: {
-        //To do - Refatorar gerador
+        //To do - Refatorar IF's
         gerador() {
 
-            //Verifica se tem um nivel e categoria selecionado. Se não emite um alerta
+            //Verifica se tem um nivel ou categoria selecionado. Se não emite um alerta
             if (this.checkedNivel == false || this.checkedCategoria == false) {
                 alert("Você deve selecionar um nível e uma categoria.");
             }
@@ -76,71 +131,119 @@ var app = new Vue({
 
             //Nivel simples
             if (this.checkedNivel == "simples" && this.checkedCategoria == "jogos") {
-                this.projetoGerado = this.simples.jogos[Math.floor(Math.random() * this.simples.jogos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.simples.jogos.titulo.length);
+                this.projetoGerado = this.simples.jogos.titulo[numAleatorio];
+                this.projetoDescricao = this.simples.jogos.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "simples" && this.checkedCategoria == "sites") {
-                this.projetoGerado = this.simples.sites[Math.floor(Math.random() * this.simples.sites.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.simples.sites.titulo.length);
+                this.projetoGerado = this.simples.sites.titulo[numAleatorio];
+                this.projetoDescricao = this.simples.sites.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "simples" && this.checkedCategoria == "softwares") {
-                this.projetoGerado = this.simples.softwares[Math.floor(Math.random() * this.simples.softwares.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.simples.softwares.titulo.length);
+                this.projetoGerado = this.simples.softwares.titulo[numAleatorio];
+                this.projetoDescricao = this.simples.softwares.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "simples" && this.checkedCategoria == "aplicativos") {
-                this.projetoGerado = this.simples.aplicativos[Math.floor(Math.random() * this.simples.aplicativos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.simples.aplicativos.titulo.length);
+                this.projetoGerado = this.simples.aplicativos.titulo[numAleatorio];
+                this.projetoDescricao = this.simples.aplicativos.descricao[numAleatorio];
+
             }
             //Nivel mediano
             else if (this.checkedNivel == "mediano" && this.checkedCategoria == "jogos") {
-                this.projetoGerado = this.mediano.jogos[Math.floor(Math.random() * this.mediano.jogos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.mediano.jogos.titulo.length);
+                this.projetoGerado = this.mediano.jogos.titulo[numAleatorio];
+                this.projetoDescricao = this.mediano.jogos.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "mediano" && this.checkedCategoria == "sites") {
-                this.projetoGerado = this.mediano.sites[Math.floor(Math.random() * this.mediano.sites.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.mediano.sites.titulo.length);
+                this.projetoGerado = this.mediano.sites.titulo[numAleatorio];
+                this.projetoDescricao = this.mediano.sites.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "mediano" && this.checkedCategoria == "softwares") {
-                this.projetoGerado = this.mediano.softwares[Math.floor(Math.random() * this.mediano.softwares.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.mediano.softwares.titulo.length);
+                this.projetoGerado = this.mediano.softwares.titulo[numAleatorio];
+                this.projetoDescricao = this.mediano.softwares.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "mediano" && this.checkedCategoria == "aplicativos") {
-                this.projetoGerado = this.mediano.aplicativos[Math.floor(Math.random() * this.mediano.aplicativos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.mediano.aplicativos.titulo.length);
+                this.projetoGerado = this.mediano.aplicativos.titulo[numAleatorio];
+                this.projetoDescricao = this.mediano.aplicativos.descricao[numAleatorio];
+
             }
             //Nivel avancado
             else if (this.checkedNivel == "avancado" && this.checkedCategoria == "jogos") {
-                this.projetoGerado = this.avancado.jogos[Math.floor(Math.random() * this.avancado.jogos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.avancado.jogos.titulo.length);
+                this.projetoGerado = this.avancado.jogos.titulo[numAleatorio];
+                this.projetoDescricao = this.avancado.jogos.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "avancado" && this.checkedCategoria == "sites") {
-                this.projetoGerado = this.avancado.sites[Math.floor(Math.random() * this.avancado.sites.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.avancado.sites.titulo.length);
+                this.projetoGerado = this.avancado.sites.titulo[numAleatorio];
+                this.projetoDescricao = this.avancado.sites.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "avancado" && this.checkedCategoria == "softwares") {
-                this.projetoGerado = this.avancado.softwares[Math.floor(Math.random() * this.avancado.softwares.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.avancado.softwares.titulo.length);
+                this.projetoGerado = this.avancado.softwares.titulo[numAleatorio];
+                this.projetoDescricao = this.avancado.softwares.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "avancado" && this.checkedCategoria == "aplicativos") {
-                this.projetoGerado = this.avancado.aplicativos[Math.floor(Math.random() * this.avancado.aplicativos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.avancado.aplicativos.titulo.length);
+                this.projetoGerado = this.avancado.aplicativos.titulo[numAleatorio];
+                this.projetoDescricao = this.avancado.aplicativos.descricao[numAleatorio];
+
             }
             //Nivel godLevel
             else if (this.checkedNivel == "godLevel" && this.checkedCategoria == "jogos") {
-                this.projetoGerado = this.godLevel.jogos[Math.floor(Math.random() * this.godLevel.jogos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.godLevel.jogos.titulo.length);
+                this.projetoGerado = this.godLevel.jogos.titulo[numAleatorio];
+                this.projetoDescricao = this.godLevel.jogos.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "godLevel" && this.checkedCategoria == "sites") {
-                this.projetoGerado = this.godLevel.sites[Math.floor(Math.random() * this.godLevel.sites.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.godLevel.sites.titulo.length);
+                this.projetoGerado = this.godLevel.sites.titulo[numAleatorio];
+                this.projetoDescricao = this.godLevel.sites.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "godLevel" && this.checkedCategoria == "softwares") {
-                this.projetoGerado = this.godLevel.softwares[Math.floor(Math.random() * this.godLevel.softwares.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.godLevel.softwares.titulo.length);
+                this.projetoGerado = this.godLevel.softwares.titulo[numAleatorio];
+                this.projetoDescricao = this.godLevel.softwares.descricao[numAleatorio];
+
             }
             else if (this.checkedNivel == "godLevel" && this.checkedCategoria == "aplicativos") {
-                this.projetoGerado = this.godLevel.aplicativos[Math.floor(Math.random() * this.godLevel.aplicativos.length)];
-                
+
+                numAleatorio = Math.floor(Math.random() * this.godLevel.aplicativos.titulo.length);
+                this.projetoGerado = this.godLevel.aplicativos.titulo[numAleatorio];
+                this.projetoDescricao = this.godLevel.aplicativos.descricao[numAleatorio];
+
             }
         }
     }
